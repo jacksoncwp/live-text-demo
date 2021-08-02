fe4cefe86e65a9e7f1b70787cd4da6ba1be026ed
Jackson Chung <jackson.chung@gogotech.hk>
2021-08-02 16:51:26 +0800

    add camera keyboard

---
+++ Live Text Address/Keyboard/CameraKeyboard.swift
@@ -0,0 +1,103 @@
+//
+//  CameraKeyboard.swift
+//  Live Text Address
+//
+//  Created by Jackson Chung on 2/8/2021.
+//
+
+import AVFoundation
+import UIKit
+
+class CameraKeyboard: UIView {
+
+    private lazy var aimContainerView: UIView = {
+        let view = UIView(frame: self.frame)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red // TODO: update
+        view.alpha = 0.7
+
+        return view
+    } ()
+
+    init() {
+        super.init(frame: .init(x: 0, y: 0, width: 320, height: 300))
+        commonInit()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+
+    private func commonInit() {
+        backgroundColor = .white
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+
+        addSubview(aimContainerView)
+        NSLayoutConstraint.activate([
+            aimContainerView.topAnchor.constraint(equalTo: topAnchor),
+            aimContainerView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            aimContainerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            aimContainerView.trailingAnchor.constraint(equalTo: trailingAnchor),
+        ])
+        addAim()
+    }
+
+    private func addAim() {
+        let upperView = UIView()
+        let lowerView = UIView()
+        let leftView = UIView()
+        let rightView = UIView()
+
+        let views: [UIView] = [upperView, lowerView, leftView, rightView]
+        views.forEach {
+            $0.backgroundColor = UIColor(red: 0.00, green: 0.53, blue: 0.75, alpha: 1.00)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            aimContainerView.addSubview($0)
+        }
+
+        NSLayoutConstraint.activate([
+            upperView.bottomAnchor.constraint(equalTo: aimContainerView.centerYAnchor, constant: -4),
+            upperView.centerXAnchor.constraint(equalTo: aimContainerView.centerXAnchor),
+            upperView.heightAnchor.constraint(equalToConstant: 16),
+            upperView.widthAnchor.constraint(equalToConstant: 2),
+
+            lowerView.topAnchor.constraint(equalTo: aimContainerView.centerYAnchor, constant: 4),
+            lowerView.centerXAnchor.constraint(equalTo: aimContainerView.centerXAnchor),
+            lowerView.heightAnchor.constraint(equalToConstant: 16),
+            lowerView.widthAnchor.constraint(equalToConstant: 2),
+
+            leftView.trailingAnchor.constraint(equalTo: aimContainerView.centerXAnchor, constant: -4),
+            leftView.centerYAnchor.constraint(equalTo: aimContainerView.centerYAnchor),
+            leftView.heightAnchor.constraint(equalToConstant: 2),
+            leftView.widthAnchor.constraint(equalToConstant: 16),
+
+            rightView.leadingAnchor.constraint(equalTo: aimContainerView.centerXAnchor, constant: 4),
+            rightView.centerYAnchor.constraint(equalTo: aimContainerView.centerYAnchor),
+            rightView.heightAnchor.constraint(equalToConstant: 2),
+            rightView.widthAnchor.constraint(equalToConstant: 16),
+        ])
+    }
+
+    @objc
+    private func keyboardWillShow(_ notification: UIKit.Notification) {
+        guard
+            let userInfo = notification.userInfo,
+            let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
+        else {
+            return
+        }
+        frame = .init(origin: .zero, size: keyboardFrame.size)
+    }
+}
--- Live Text Address/ViewController.swift
+++ Live Text Address/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     } ()
 
     private lazy var keyboardToolbar: UIToolbar = {
-        let toolbar = UIToolbar()
+        let toolbar = UIToolbar(frame: .init(x: 0, y: 0, width: 320, height: 44))
         let flexible = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
         let cameraBtn = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(cameraDidTap))
         let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneDidTap))
@@ -30,6 +30,23 @@ class ViewController: UIViewController {
         toolbar.sizeToFit()
 
         return toolbar
+    } ()
+
+    private lazy var cameraToolbar: UIToolbar = {
+        let toolbar = UIToolbar(frame: .init(x: 0, y: 0, width: 320, height: 44))
+        let flexible = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: self, action: nil)
+        let keyboardBtn = UIBarButtonItem(title: "Keyboard", style: .plain, target: self, action: #selector(keyboardDidTap))
+        let doneBtn = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(doneDidTap))
+
+        toolbar.items = [flexible, keyboardBtn, doneBtn]
+        toolbar.sizeToFit()
+
+        return toolbar
+    } ()
+
+    private var cameraInputView: CameraKeyboard = {
+        let view = CameraKeyboard()
+        return view
     } ()
 
     override func viewDidLoad() {
@@ -42,17 +59,31 @@ class ViewController: UIViewController {
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(48)
         }
+        searchAddressTextField.addTarget(self, action: #selector(addressDidChange), for: .editingChanged)
         searchAddressTextField.inputAccessoryView = keyboardToolbar
     }
 
     @objc
     private func cameraDidTap() {
-        // TODO: show camera
+        searchAddressTextField.inputAccessoryView = cameraToolbar
+        searchAddressTextField.inputView = cameraInputView
+        searchAddressTextField.reloadInputViews()
+    }
+
+    @objc
+    private func keyboardDidTap() {
+        searchAddressTextField.inputAccessoryView = keyboardToolbar
+        searchAddressTextField.inputView = nil
+        searchAddressTextField.reloadInputViews()
     }
 
     @objc
     private func doneDidTap() {
         searchAddressTextField.resignFirstResponder()
     }
-}
 
+    @objc
+    private func addressDidChange() {
+        print("search address: \(searchAddressTextField.text ?? "")")
+    }
+}
